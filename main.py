c2f33fb6f3a65ec7dc112fd9c26a75d4a410b43e
Red-dino <alonzo.gsmst@gmail.com>
2026-01-02 19:46:31 -0800

    Call Gemini and save and run the program it returns.

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import pygame
 import importlib
+import time
 from generated.helpers import Render, Input, Sound
+from google import genai
+from google.genai import types
 
 pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.init()
+
+def get_prompt():
+    string = ''
+    with open("prompt.txt", "r") as f:
+        string = f.read()
+    return string
+
+def write_program(text):
+    name = str(int(time.time()))
+    text = text.replace('```python', '')
+    text = text.replace('```', '')
+    with open("generated/" + name + ".py", 'w') as f:
+        f.write('import math\n')
+        f.write('import random\n')
+        f.write('from generated.helpers import Render, Input, Sound\n\n')
+
+        f.write(text)
+    return name
+
+def get_and_load_program(chat, prompt):
+    response = chat.send_message(prompt)
+    name = write_program(response.text)
+    # name = '1767410044'
+    module = importlib.import_module("generated." + name)
+    program = module.Program()
+    print(program.get_instructions())
+    print(program.get_next_idea())
+    return program
 
 def main():
     size = (800, 600)
@@ -11,18 +42,23 @@ def main():
     clock = pygame.time.Clock()
     running = True
 
-    module = importlib.import_module("generated.example")
-    program = module.Program()
-    print(program.get_instructions())
+    prompt = get_prompt()   
+
+    client = genai.Client()
+    chat = client.chats.create(model="gemini-2.5-flash-lite", config=types.GenerateContentConfig(system_instruction=prompt))
+
+    program = get_and_load_program(chat, "flying kite")
 
     dt = 0
-
     while running:
         # update
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
+                if pygame.key.name(event.key) == 'g':
+                    program = get_and_load_program(chat, "Keep dreaming.")
+
                 Input.key_down(pygame.key.name(event.key))
             elif event.type == pygame.KEYUP:
                 Input.key_up(pygame.key.name(event.key))
